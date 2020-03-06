c9b55cfda48591ddb5fd3f91a8ba5d133d9a8aa0
prashanth-23 <prashanthreddy9948379663@gmail.com>
2020-03-06 13:24:06 +0530

    added resources routing convention

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.
              join("\n")
@@ -6,7 +8,7 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = params[:due_date]
+    due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
