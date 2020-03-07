31e1d626b787c7dde99091eb0090776b9273b11a
prashanth-23 <prashanthreddy9948379663@gmail.com>
2020-03-07 18:37:50 +0530

    ability to add a new user by writing create method in usercontrollers.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,17 @@ class UsersController < ApplicationController
     render plain: User.order(:name).map { |user| user.to_pleasant_string }.
              join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "new user is created with id #{new_user.id}"
+    render plain: response_text
+  end
 end
