22493df590c20bf280642606213ddc4aba87b4ac
prashanth-23 <prashanthreddy9948379663@gmail.com>
2020-04-13 17:14:52 +0530

    able to signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    user = User.create!(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    redirect_to new_sessions_path
+    session[:current_user_id] = user.id
+    redirect_to todos_path
   end
 
   def check
